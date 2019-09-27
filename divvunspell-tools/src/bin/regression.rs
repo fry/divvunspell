c4dced32e603d4fe7e3554bdca718d457e7504b0
Brendan Molloy <brendan@technocreatives.com>
2019-09-27 10:27:20 +0200

    Add rudimentary regressions

--- divvunspell-tools/src/bin/regression.rs
+++ divvunspell-tools/src/bin/regression.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::path::PathBuf;
 use structopt::StructOpt;
 use divvunspell::report::*;
-use structdiff::{Apply, Diff};
+use structdiff::{Apply, Diff, Field, types::VecAction};
 
 #[derive(Debug, StructOpt)]
 struct Opts {
@@ -12,6 +12,63 @@ struct Opts {
 
     #[structopt(parse(from_os_str))]
     diff_b: PathBuf,
+}
+
+#[derive(Debug)]
+enum Difference {
+    Position { previous: u8, current: u8 }
+}
+
+fn process_actions(results: &Vec<AccuracyResult>, actions: Vec<VecAction<AccuracyResult>>) -> Result<(), Box<dyn Error>> {
+    // let mut x = 0;
+    
+    let actions = actions.into_iter().filter_map(|action| {
+        use VecAction::*;
+
+        match action {
+            Set(index, field) => {
+                Some((index, field))
+            },
+            _ => None
+            // Push
+            // Truncate
+            // Append
+        }
+    });
+
+    actions.for_each(|(index, field)| {
+        use Field::*;
+
+        let result = &results[index];
+
+        match field {
+            None => {},
+            Set(value) => {
+
+            },
+            Changes(changes) => {
+                match changes.position {
+                    None => {},
+                    Changes(changes) => {
+                        println!("{}: {:?} -> {:?}", result.input, result.position, changes);
+                    }
+                    Set(value) => {
+                        println!("{}: {:?} -> {:?}", result.input, result.position, value);
+                    },
+                    Actions(actions) => {
+                        println!("{}: {:?} -> {:?}", result.input, result.position, actions);
+                    }
+                }
+            }
+            Actions(actions) => {
+
+            },
+        }
+    });
+
+    // println!("\n{}/{}", x, results.len());
+    
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -24,10 +81,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     let report_b: Report = serde_json::from_reader(file_b)?;
     let changeset = report_a.results.changeset(&report_b.results);
 
-    let mut results = report_b.results;
+    match changeset {
+        Field::None => {
+            println!("No changes.");
+            Ok(())    
+        },
+        Field::Actions(actions) => {
+            process_actions(&report_a.results, actions)
+        },
+        _ => Ok(())
+    }
 
     // println!("{:#?}", &changeset);
-    changeset.apply(&mut results);
-    assert_eq!(&report_a.results, &results);
-    Ok(())
+    // changeset.apply(&mut results);
+    // assert_eq!(&report_a.results, &results);
+    // Ok(())
 }
